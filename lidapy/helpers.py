fe670b84a6ad3555840bfa1ba669fe463039c398
Pulin Agrawal <pulinagrawal@gmail.com>
2024-12-28 14:04:56 -0500

    push helpers to go with previous commit

--- lidapy/helpers.py
+++ lidapy/helpers.py
@@ -1,6 +1,10 @@
+from math import comb
 import numpy as np
 from scipy.spatial.distance import cosine as cosine_distance
 import ollama
+import randomname
+
+randomname = lambda: randomname.get_name()
 
 class Node:
     def __init__(self, vector, text, activation, tags=None, links=None):
@@ -31,7 +35,7 @@ def get_most_similar_node(focus_vector, nodes):
             max_similarity = similarity
     return most_similar_node, max_similarity
 
-def combine_nodes(nodes):
+def _text_combine(nodes):
     # Concatenate text from all nodes
     combined_text = " ".join(node.text for node in nodes)
     
@@ -43,8 +47,23 @@ def combine_nodes(nodes):
     # combined activation is the average of the activations of the nodes being combined
     average_activation = sum(node.activation for node in nodes) / len(nodes)
     combined_node = Node(combined_vector, combined_text, activation=average_activation)
-    
     return combined_node
+    
+
+average_embedding = lambda nodes: np.mean([node.vector for node in nodes], axis=0)
+
+def _average_embedding_combine(nodes):
+    combined_vector = average_embedding(nodes)
+    combined_text = "\n and \n".join(node.text for node in nodes)
+    average_activation = sum(node.activation for node in nodes) / len(nodes)
+    combined_node = Node(combined_vector, combined_text, activation=average_activation)
+    return combined_node
+
+def combine_nodes(nodes, method='text'):
+    if method == 'text':
+        return _text_combine(nodes)
+    elif method == 'average':
+        return _average_embedding_combine(nodes) 
 
 # Assume EmbeddingModel is a placeholder for your actual embedding model with an encode method
 class EmbeddingModel:
@@ -59,13 +78,9 @@ class EmbeddingModel:
         return ollama.generate(self.model_name, prompt)
 
 
-# Usage
 embedding_model = EmbeddingModel()
 embed = lambda text: embedding_model.encode(text)
 generate = lambda prompt: embedding_model.generate(prompt)
 create_node = lambda text, activation=1.0: Node(embed(text), text, activation=activation)
-nodes_to_combine = [Node(np.array([0.1, 0.2, 0.3]), "Hello, world!", 1.0),
-                    Node(np.array([0.4, 0.5, 0.6]), "Goodbye, world!", 1.0)]
-combined_node = combine_nodes(nodes_to_combine)
 get_similarity = lambda x, y: 1-cosine_distance(x, y)
 # get_similarity = lambda x, y: 1/(1+np.sqrt(np.linalg.norm(x - y)))
